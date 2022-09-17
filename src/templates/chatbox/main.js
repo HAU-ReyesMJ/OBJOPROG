bfd499f3b283aa3f10e8f05ff7bfeb56907a7e03
immerk <xancorpse19@gmail.com>
2022-09-17 11:38:34 +0800

    still trying some popup chatbox

---
+++ src/templates/chatbox/main.js
@@ -0,0 +1,14 @@
+const emojiBtn = document.querySelector('#emoji-btn');
+const picker = new EmojiButton();
+
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    picker.on('emoji', emoji => {
+        document.querySelector('input').value += emoji;
+    });
+
+    emojiBtn.addEventListener('click', () => {
+        picker.togglePicker(emojiBtn);
+    });
+});
